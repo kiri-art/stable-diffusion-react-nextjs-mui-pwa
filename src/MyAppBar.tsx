db315d19698fdc12e5cadb9c1c0d05248779bd56
Gadi Cohen <dragon@wastelands.net>
2022-08-31 05:39:38 +0300

    MyAppBar

---
+++ src/MyAppBar.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { AppBar, Box, Toolbar, Typography, Button, IconButton, MenuIcon } from "@mui";
+
+export default function MyAppBar({ title }: { title: string }) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            {title}
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
