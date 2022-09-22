ceb8e469bfa0e924bfd6bff22c9e4c15133e3788
Gadi Cohen <dragon@wastelands.net>
2022-09-22 06:34:03 +0300

    InputSlider: ignore clicks on rail, only allow thumb drag

--- src/InputSlider.tsx
+++ src/InputSlider.tsx
@@ -3,7 +3,7 @@ import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Slider from "@mui/material/Slider";
+import UnstyledSlider from "@mui/material/Slider";
 import MuiInput from "@mui/material/Input";
 import HelpIcon from "@mui/icons-material/HelpOutline";
 import Tooltip from "@mui/material/Tooltip";
@@ -12,6 +12,13 @@ import { SettingsBackupRestore } from "@mui/icons-material";
 
 const Input = styled(MuiInput)`
   width: 42px;
+`;
+
+const Slider = styled(UnstyledSlider)`
+  pointer-events: none !important;
+  .MuiSlider-thumb {
+    pointer-events: all !important;
+  }
 `;
 
 /**
