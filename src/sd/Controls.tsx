81fe83da2a3d1d14e51d718176822b72c46fe28d
Gadi Cohen <dragon@wastelands.net>
2022-10-05 09:31:14 +0100

    controls: waifu diffusion epoch09 label

--- src/sd/Controls.tsx
+++ src/sd/Controls.tsx
@@ -684,7 +684,7 @@ function ModelSelect({
           >
             <ModelMenuItem
               value="hakurei/waifu-diffusion-v1-3"
-              desc={t`New!  Epoch 8 (will be 10), Oct 4`}
+              desc={t`New!  Epoch 9 (will be 10), Oct 5`}
             />
           </MenuItem>
 
