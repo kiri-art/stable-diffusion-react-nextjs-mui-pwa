c79171d3ee7bf73ed666887131f95d222097fc13
Gadi Cohen <dragon@wastelands.net>
2022-10-15 14:40:34 +0100

    s/[_id] fix for when db is not loaded yet

--- pages/s/[_id].tsx
+++ pages/s/[_id].tsx
@@ -12,6 +12,15 @@ export default function StarredItem() {
   const { _id } = router.query;
 
   const item = useGongoOne((db) => db.collection("stars").find({ _id }));
+  if (!item) return <div>Loading...</div>;
+
+  const modelInputs = item.modelInputs;
+
+  // TODO, need to store w/h in result for other pipelines that don't specify.
+  const aspectRatio =
+    modelInputs.width && modelInputs.height
+      ? modelInputs.width + "/" + modelInputs.height
+      : undefined;
 
   return (
     <Box>
@@ -19,9 +28,10 @@ export default function StarredItem() {
       <Container sx={{ my: 2 }}>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          alt={item.modelInputs.prompt}
+          alt={modelInputs.prompt}
           src={"/api/file?id=" + strObjectId(item.files.output)}
-          style={{ maxWidth: "100%", border: "1px solid black" }}
+          style={{ maxWidth: "100%", border: "1px solid black", aspectRatio }}
+          width="100%"
         />
         <p>
           By: <Link href={"/p/" + item.userId}>{item.userId}</Link> (coming
@@ -30,16 +40,16 @@ export default function StarredItem() {
         <p>Liked by: {item.likes} users</p>
 
         <p>
-          <Trans>Prompt</Trans>: {item.modelInputs.prompt}
+          <Trans>Prompt</Trans>: {modelInputs.prompt}
         </p>
         <p>
-          <Trans>Negative Prompt</Trans>: {item.modelInputs.negative_prompt}
+          <Trans>Negative Prompt</Trans>: {modelInputs.negative_prompt}
         </p>
         <p>
-          <Trans>CFG</Trans>: {item.modelInputs.guidance_scale}
+          <Trans>CFG</Trans>: {modelInputs.guidance_scale}
         </p>
         <p>
-          <Trans>Steps</Trans>: {item.modelInputs.num_inference_steps}
+          <Trans>Steps</Trans>: {modelInputs.num_inference_steps}
         </p>
       </Container>
     </Box>
