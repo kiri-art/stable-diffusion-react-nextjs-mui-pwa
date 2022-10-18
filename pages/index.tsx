8bd087802dac9dcca7bad44715f73623c49b537d
Gadi Cohen <dragon@wastelands.net>
2022-10-18 08:48:03 +0100

    index: nsfw filter, most recent/popular headings

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,9 @@ import {
   FormControlLabel,
   FormGroup,
   Switch,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
 } from "@mui/material";
 
 import Link from "../src/Link";
@@ -16,15 +19,19 @@ import MyAppBar from "../src/MyAppBar";
 import Copyright from "../src/Copyright";
 import { useGongoLive, useGongoSub } from "gongo-client-react";
 import Starred from "../src/Starred";
+import useOver18 from "../src/lib/useOver18";
 
 const Home: NextPage = () => {
   const [nsfw, setNsfw] = React.useState(true);
+  const [show, setShow] = React.useState("recent");
+  const over18 = useOver18();
 
   const query: Record<string, unknown> = { deleted: { $ne: true } };
+  const sortField = show === "recent" ? "date" : "likes";
   if (nsfw) query["callInputs.safety_checker"] = true;
 
   const items = useGongoLive((db) =>
-    db.collection("stars").find(query).sort("date", "desc").limit(100)
+    db.collection("stars").find(query).sort(sortField, "desc").limit(100)
   );
   useGongoSub("stars");
 
@@ -37,36 +44,61 @@ const Home: NextPage = () => {
             <Trans>Start Creating</Trans>
           </Button>
         </Box>
-
         <br />
+        <Box sx={{ textAlign: "center" }}>
+          <Typography variant="h6">
+            <Trans>Community</Trans>
+          </Typography>
+          <Box sx={{ fontSize: "80%" }}>
+            <Trans>
+              <span style={{ whiteSpace: "nowrap" }}>
+                Star images from your <Link href="/history">history</Link>
+              </span>{" "}
+              <span style={{ whiteSpace: "nowrap" }}>
+                and have them appear here too!
+              </span>
+            </Trans>
+          </Box>
+          <br />
+        </Box>
+        {over18 && (
+          <FormGroup sx={{ alignItems: "center" }}>
+            <FormControlLabel
+              sx={{ mr: 0 }}
+              control={
+                <Switch
+                  checked={nsfw}
+                  onChange={(event) => setNsfw(event.target.checked)}
+                />
+              }
+              label={
+                <Box>
+                  <Trans>NSFW Filter</Trans>
+                </Box>
+              }
+            />
+          </FormGroup>
+        )}
 
         <Box sx={{ textAlign: "center" }}>
-          <Trans>Recently Starred</Trans>
-          <br />
-          <Box sx={{ fontSize: "80%" }}>
-            <Trans>Star your images to have them appear here too!</Trans>
-          </Box>
+          <ToggleButtonGroup
+            color="primary"
+            value={show}
+            exclusive
+            size="small"
+            onChange={(_event, newValue) => newValue && setShow(newValue)}
+            aria-label="Platform"
+            sx={{ fontSize: "80%" }}
+          >
+            <ToggleButton value="recent">
+              <Trans>Most Recent</Trans>
+            </ToggleButton>
+            <ToggleButton value="popular">
+              <Trans>Most Popular</Trans>
+            </ToggleButton>
+          </ToggleButtonGroup>
         </Box>
-
-        <FormGroup sx={{ alignItems: "center" }}>
-          <FormControlLabel
-            sx={{ mr: 0 }}
-            control={
-              <Switch
-                checked={nsfw}
-                onChange={(event) => setNsfw(event.target.checked)}
-              />
-            }
-            label={
-              <Box>
-                <Trans>NSFW Filter</Trans>
-              </Box>
-            }
-          />
-        </FormGroup>
-
         <Starred items={items} />
-
         <Copyright />
       </Container>
     </>
