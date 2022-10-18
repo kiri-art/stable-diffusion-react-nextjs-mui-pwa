8f91b607d55986451bc4fea20c4154e1ff2bdc4c
Gadi Cohen <dragon@wastelands.net>
2022-10-18 07:31:59 +0100

    index: nsfw filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,14 @@ import * as React from "react";
 import type { NextPage } from "next";
 import { t, Trans } from "@lingui/macro";
 
-import { Box, Button, Container } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  FormControlLabel,
+  FormGroup,
+  Switch,
+} from "@mui/material";
 
 import Link from "../src/Link";
 import MyAppBar from "../src/MyAppBar";
@@ -11,12 +18,13 @@ import { useGongoLive, useGongoSub } from "gongo-client-react";
 import Starred from "../src/Starred";
 
 const Home: NextPage = () => {
+  const [nsfw, setNsfw] = React.useState(true);
+
+  const query: Record<string, unknown> = { deleted: { $ne: true } };
+  if (nsfw) query["callInputs.safety_checker"] = true;
+
   const items = useGongoLive((db) =>
-    db
-      .collection("stars")
-      .find({ deleted: { $ne: true } })
-      .sort("date", "desc")
-      .limit(100)
+    db.collection("stars").find(query).sort("date", "desc").limit(100)
   );
   useGongoSub("stars");
 
@@ -39,6 +47,23 @@ const Home: NextPage = () => {
             <Trans>Star your images to have them appear here too!</Trans>
           </Box>
         </Box>
+
+        <FormGroup sx={{ alignItems: "center" }}>
+          <FormControlLabel
+            sx={{ mr: 0 }}
+            control={
+              <Switch
+                checked={nsfw}
+                onChange={(event) => setNsfw(event.target.checked)}
+              />
+            }
+            label={
+              <Box>
+                <Trans>NSFW Filter</Trans>
+              </Box>
+            }
+          />
+        </FormGroup>
 
         <Starred items={items} />
 
