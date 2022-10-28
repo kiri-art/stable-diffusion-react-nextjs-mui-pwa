b846cf5dbcf04341cf56c7b91f77313be4d3d894
Gadi Cohen <dragon@wastelands.net>
2022-10-28 11:06:03 +0100

    /p/[_id] now re-uses /[_id] profile page

--- pages/p/[_id].tsx
+++ pages/p/[_id].tsx
@@ -1,28 +1,6 @@
 import React from "react";
-import { useRouter } from "next/router";
-import { useGongoLive, useGongoSub } from "gongo-client-react";
-import { Box, Container } from "@mui/material";
-import { t } from "@lingui/macro";
+import Profile from "../[_id]";
 
-import Starred from "../../src/Starred";
-import MyAppBar from "../../src/MyAppBar";
-
-export default function Profile() {
-  const router = useRouter();
-  const { _id: userId } = router.query;
-
-  const items = useGongoLive((db) =>
-    db.collection("stars").find({ userId }).sort("date", "desc")
-  );
-
-  useGongoSub("stars", { userId });
-
-  return (
-    <Box>
-      <MyAppBar title={t`Profile`} />
-      <Container sx={{ my: 2 }}>
-        <Starred items={items} />
-      </Container>
-    </Box>
-  );
+export default function ProfileFromUserId() {
+  return <Profile />;
 }
