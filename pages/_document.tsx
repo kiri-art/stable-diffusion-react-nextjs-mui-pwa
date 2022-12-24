015da154c906721e6a7b83f51400a4def0ac99c3
Gadi Cohen <dragon@wastelands.net>
2022-12-24 13:32:03 +0200

    chore(app): new logo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,6 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/favicon.ico" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
@@ -45,32 +44,12 @@ export default class MyDocument extends Document {
           />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta
-            name="msapplication-config"
-            content="/icons/browserconfig.xml"
-          />
-          <meta name="msapplication-TileColor" content="#2B5797" />
-          <meta name="msapplication-tap-highlight" content="no" />
-          <meta name="theme-color" content="#000000" />
 
-          {/*
-          <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/icons/touch-icon-ipad.png"
-          />
           <link
             rel="apple-touch-icon"
             sizes="180x180"
-            href="/icons/touch-icon-iphone-retina.png"
+            href="/icons/apple-touch-icon.png"
           />
-          <link
-            rel="apple-touch-icon"
-            sizes="167x167"
-            href="/icons/touch-icon-ipad-retina.png"
-          />
-
           <link
             rel="icon"
             type="image/png"
@@ -83,44 +62,41 @@ export default class MyDocument extends Document {
             sizes="16x16"
             href="/icons/favicon-16x16.png"
           />
+          <link rel="manifest" href="/icons/site.webmanifest" />
           <link
             rel="mask-icon"
             href="/icons/safari-pinned-tab.svg"
             color="#5bbad5"
           />
-          <meta name="twitter:card" content="summary" />
-          <meta name="twitter:url" content="https://yourdomain.com" />
-          <meta name="twitter:title" content="PWA App" />
+          <link rel="shortcut icon" href="/favicon.ico" />
+          <meta name="msapplication-TileColor" content="#ffc40d" />
+          <meta name="msapplication-config" content="/browserconfig.xml" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          <meta name="theme-color" content="#ffffff" />
+          <meta name="twitter:card" content="Diffusion Image Generation" />
+          <meta name="twitter:url" content="https://kiri.art" />
+          <meta name="twitter:title" content="Kiri Art" />
           <meta
             name="twitter:description"
             content="Best PWA App in the world"
           />
           <meta
             name="twitter:image"
-            content="https://yourdomain.com/icons/android-chrome-192x192.png"
+            content="https://kiri.art/icons/android-chrome-192x192.png"
           />
-          <meta name="twitter:creator" content="@DavidWShadow" />
+          <meta name="twitter:creator" content="@gadicc" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="PWA App" />
-          <meta property="og:description" content="Best PWA App in the world" />
-          <meta property="og:site_name" content="PWA App" />
-          <meta property="og:url" content="https://yourdomain.com" />
+          <meta property="og:title" content="Kiri.Art" />
+          <meta
+            property="og:description"
+            content="Diffusion Image Generation"
+          />
+          <meta property="og:site_name" content="Kiri.Art" />
+          <meta property="og:url" content="https://kiri.art/" />
           <meta
             property="og:image"
-            content="https://yourdomain.com/icons/apple-touch-icon.png"
+            content="https://kiri.art/icons/apple-touch-icon.png"
           />
-          */}
-
-          {/* <!-- apple splash screen images -->
-            <!--
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_2048.png' sizes='2048x2732' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_1668.png' sizes='1668x2224' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_1536.png' sizes='1536x2048' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_1125.png' sizes='1125x2436' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_1242.png' sizes='1242x2208' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_750.png' sizes='750x1334' />
-            <link rel='apple-touch-startup-image' href='/images/apple_splash_640.png' sizes='640x1136' />
-          --> */}
         </Head>
         <body>
           <Main />
