8dfc21e693dd0b1dca5ff2157ecb638d29b6dbec
Gadi Cohen <dragon@wastelands.net>
2023-01-03 10:21:14 +0200

    fix(bananaUrl): default to banana for undefined

--- src/lib/bananaUrl.ts
+++ src/lib/bananaUrl.ts
@@ -1,5 +1,5 @@
 export default function bananaUrl(provider_id: 1 | 2) {
-  if (provider_id === 1) return "https://api.banana.dev";
+  if (provider_id === 1 || !provider_id) return "https://api.banana.dev";
   // if (provider_id === 2) return "https://api.kiri.art/api";
   if (provider_id === 2) return "https://api2.kiri.art";
   return "UNKNOWN PROVIDER " + provider_id;
