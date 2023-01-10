d84328d093cbca6c793560616ac303f63b032c40
Gadi Cohen <dragon@wastelands.net>
2023-01-10 13:13:49 +0200

    fix(bananaUrl): use api-ams.kiri.art for prov2 (was: api2)

--- src/lib/bananaUrl.ts
+++ src/lib/bananaUrl.ts
@@ -1,6 +1,6 @@
 export default function bananaUrl(provider_id: 1 | 2) {
   if (provider_id === 1 || !provider_id) return "https://api.banana.dev";
   // if (provider_id === 2) return "https://api.kiri.art/api";
-  if (provider_id === 2) return "https://api2.kiri.art";
+  if (provider_id === 2) return "https://api-ams.kiri.art";
   return "UNKNOWN PROVIDER " + provider_id;
 }
