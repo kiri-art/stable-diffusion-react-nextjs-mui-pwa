08cbd6700ce9f39b22d32973d6389409f66ca3a7
Gadi Cohen <dragon@wastelands.net>
2023-01-24 14:04:18 +0200

    feat(index): no scroll on query update; explicit & freeform filters

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,17 @@ import { t, Trans } from "@lingui/macro";
 import {
   Box,
   Button,
+  Checkbox,
   Container,
   FormControlLabel,
   FormGroup,
+  IconButton,
+  InputAdornment,
   Switch,
+  TextField,
   ToggleButton,
   ToggleButtonGroup,
+  Tooltip,
   Typography,
 } from "@mui/material";
 
@@ -21,8 +26,77 @@ import { useGongoLive, useGongoSub } from "gongo-client-react";
 import Starred from "../src/Starred";
 import useOver18 from "../src/lib/useOver18";
 import { useRouter } from "next/router";
-import { GridView, Splitscreen } from "@mui/icons-material";
+import { Clear, GridView, Help, Splitscreen } from "@mui/icons-material";
 import { NUM_REPORTS_UNTIL_REMOVAL } from "../src/lib/constants";
+
+function TextFieldDebounced({
+  currentValue,
+  onChange,
+}: {
+  currentValue: string;
+  onChange: (value: string) => void;
+}) {
+  const timeout = React.useRef<ReturnType<typeof setTimeout> | null>(null);
+  const [value, setValue] = React.useState("");
+
+  React.useEffect(() => {
+    if (value !== currentValue) {
+      console.log(1);
+      if (timeout.current) {
+        clearTimeout(timeout.current);
+        timeout.current = null;
+      }
+      timeout.current = setTimeout(() => onChange(value), 750);
+    }
+    return () => {
+      timeout.current && clearTimeout(timeout.current);
+    };
+  }, [onChange, value, currentValue]);
+
+  return React.useMemo(
+    () => (
+      <TextField
+        placeholder={t`Filter`}
+        size="small"
+        sx={{ mt: 1, width: 400 }}
+        value={value}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          setValue(event.target.value)
+        }
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              {value != "" && (
+                <IconButton onClick={() => setValue("")} edge="end">
+                  <Clear />
+                </IconButton>
+              )}
+
+              <Tooltip
+                title={
+                  <Box>
+                    <Trans>
+                      Only show stars with matching prompts. More advanced
+                      filters coming soon. Separate multiple terms with a pipe
+                      (&quot;|&quot;) character.
+                    </Trans>
+                  </Box>
+                }
+                enterDelay={0}
+                enterTouchDelay={0}
+                leaveDelay={0}
+                leaveTouchDelay={4000}
+              >
+                <Help />
+              </Tooltip>
+            </InputAdornment>
+          ),
+        }}
+      />
+    ),
+    [value]
+  );
+}
 
 const Home: NextPage = () => {
   const router = useRouter();
@@ -31,19 +105,56 @@ const Home: NextPage = () => {
   const nsfwFilter =
     !router.query.nsfwFilter || router.query.nsfwFilter === "true";
   const setNsfw = (nsfwFilter: boolean) =>
-    router.replace({ pathname: "/", query: { ...router.query, nsfwFilter } });
+    router.replace(
+      { pathname: "/", query: { ...router.query, nsfwFilter } },
+      undefined,
+      { shallow: true, scroll: false }
+    );
 
   // const [show, setShow] = React.useState("recent");
   const show = router.query.show || "recent";
   const setShow = (show: string) =>
-    router.replace({ pathname: "/", query: { ...router.query, show } });
+    router.replace(
+      { pathname: "/", query: { ...router.query, show } },
+      undefined,
+      { shallow: true, scroll: false }
+    );
+
+  const explicit = router.query.explicit === "true" ? true : false;
+  const setExplicit = (explicit: boolean) =>
+    router.replace(
+      { pathname: "/", query: { ...router.query, explicit } },
+      undefined,
+      { shallow: true, scroll: false }
+    );
+
+  const filter =
+    typeof router.query.filter === "string" ? router.query.filter : "";
+  const setFilter = React.useCallback(
+    (filterOrEvent: string | React.ChangeEvent<HTMLInputElement>) => {
+      const filter =
+        typeof filterOrEvent === "string"
+          ? filterOrEvent
+          : filterOrEvent.target.value;
+      router.replace(
+        { pathname: "/", query: { ...router.query, filter } },
+        undefined,
+        { shallow: true, scroll: false }
+      );
+    },
+    [router]
+  );
 
   const over18 = useOver18();
 
   // const [useGrid, setUseGrid] = React.useState(true);
   const useGrid = !router.query.useGrid || router.query.useGrid === "true";
   const setUseGrid = (useGrid: boolean) =>
-    router.replace({ pathname: "/", query: { ...router.query, useGrid } });
+    router.replace(
+      { pathname: "/", query: { ...router.query, useGrid } },
+      undefined,
+      { shallow: true, scroll: false }
+    );
 
   const query: Record<string, unknown> = {};
   if (!router.query.showDeleted) query.deleted = { $ne: true };
@@ -54,10 +165,26 @@ const Home: NextPage = () => {
     ];
   const sortField = show === "recent" ? "date" : "likes";
   if (nsfwFilter) query["callInputs.safety_checker"] = true;
+  if (!nsfwFilter && !explicit)
+    query["modelInputs.prompt"] = {
+      $not: /tits|cum|dick|pussy|loli|sex|ejaculation|vagina|penis/i,
+    };
 
   const items = useGongoLive(
     (db) => db.collection("stars").find(query).sort(sortField, "desc") //.limit(100)
   );
+
+  // We don't do this as part of the gongo query because the regexp instance doesn't
+  // serialize and breaks the hasQueryChanged check (TODO in gongo)
+  const filteredItems = React.useMemo(() => {
+    const reFilter = new RegExp(filter, "i");
+
+    return items.filter((item) => {
+      const prompt = item?.modelInputs?.prompt;
+      if (prompt && prompt.match(reFilter)) return true;
+    });
+  }, [items, filter]);
+
   useGongoSub("stars");
 
   return (
@@ -87,9 +214,8 @@ const Home: NextPage = () => {
           <br />
         </Box>
         {over18 && (
-          <FormGroup sx={{ alignItems: "center" }}>
+          <FormGroup sx={{ justifyContent: "center", flexDirection: "row" }}>
             <FormControlLabel
-              sx={{ mr: 0 }}
               control={
                 <Switch
                   checked={nsfwFilter}
@@ -102,6 +228,21 @@ const Home: NextPage = () => {
                 </Box>
               }
             />
+            {!nsfwFilter && (
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={explicit}
+                    onChange={(event) => setExplicit(event.target.checked)}
+                  />
+                }
+                label={
+                  <Box>
+                    <Trans>Explicit</Trans>
+                  </Box>
+                }
+              />
+            )}
           </FormGroup>
         )}
 
@@ -139,7 +280,10 @@ const Home: NextPage = () => {
             </ToggleButton>
           </ToggleButtonGroup>
         </Box>
-        <Starred items={items} cols={useGrid ? undefined : 1} />
+        <Box sx={{ textAlign: "center" }}>
+          <TextFieldDebounced currentValue={filter} onChange={setFilter} />
+        </Box>
+        <Starred items={filteredItems} cols={useGrid ? undefined : 1} />
         <Copyright />
       </Container>
     </>
