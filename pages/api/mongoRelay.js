eed1ba8c3e3f80965bfac1028fc1558b83a35e7b
Gadi Cohen <dragon@wastelands.net>
2023-08-26 12:20:05 +0100

    fix(mongoRelay): make .js;  @ts-ignore needed locally, not in prod??

--- pages/api/mongoRelay.js
+++ pages/api/mongoRelay.js
@@ -4,5 +4,5 @@ import makeExpressRelay from "mongodb-rest-relay/lib/express";
 const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1";
 const client = new MongoClient(MONGO_URL);
 
-// @ts-expect-error: ok
+// ts fails sometimes here (but not always???) because of version mismatch
 export default makeExpressRelay((await client.connect()).db("sd-mui"));
