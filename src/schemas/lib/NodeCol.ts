6751c8f36206c125f0c0a54c368b320574674a2f
Gadi Cohen <dragon@wastelands.net>
2023-11-03 11:52:42 +0000

    fix(NodeCol): support ObjectId from both mongodb and -relay

--- src/schemas/lib/NodeCol.ts
+++ src/schemas/lib/NodeCol.ts
@@ -1,5 +1,6 @@
-import { ObjectId } from "mongodb";
+import { ObjectId as MongoObjectId } from "mongodb";
+import { ObjectId } from "../../api-lib/objectId";
 
-type NodeCol<T> = Omit<T, "_id"> & { _id: ObjectId };
+type NodeCol<T> = Omit<T, "_id"> & { _id: ObjectId | MongoObjectId };
 
 export default NodeCol;
