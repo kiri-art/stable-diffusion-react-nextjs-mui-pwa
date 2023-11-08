7f6a70534beb6c7edddb8b21cb12f8e2a0a096e6
Gadi Cohen <dragon@wastelands.net>
2023-11-08 12:14:21 +0000

    fix(deps): switch to DuCanhGH/next-pwa (app-build-manifest.json)

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require("next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   skipWaiting: false,
