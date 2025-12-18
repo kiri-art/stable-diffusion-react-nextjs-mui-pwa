312b17b1f7975651f498c9c27eef6118a84248aa
Gadi Cohen <dragon@wastelands.net>
2025-12-18 12:13:41 +0200

    fix: eslint codemod
    npx @eslint/migrate-config /home/dragon/www/projects/stable-diffusion-react-next
    which bailed last time

---
+++ eslint.config.cjs
@@ -0,0 +1,43 @@
+const {
+    defineConfig,
+} = require("eslint/config");
+
+const tsParser = require("@typescript-eslint/parser");
+const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const unusedImports = require("eslint-plugin-unused-imports");
+const js = require("@eslint/js");
+
+const {
+    FlatCompat,
+} = require("@eslint/eslintrc");
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+module.exports = defineConfig([{
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+        "unused-imports": unusedImports,
+    },
+
+    extends: compat.extends("next/core-web-vitals", "plugin:@typescript-eslint/recommended"),
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": "off",
+        "unused-imports/no-unused-imports": "error",
+
+        "unused-imports/no-unused-vars": ["warn", {
+            vars: "all",
+            varsIgnorePattern: "^_",
+            args: "after-used",
+            argsIgnorePattern: "^_",
+        }],
+    },
+}]);
