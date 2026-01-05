e337268f7364420691b25e8e8dc233f204fc176e
Gadi Cohen <dragon@wastelands.net>
2026-01-05 12:22:11 +0200

    feat(pkg): switch from jest to vitest

--- src/lib/civitai.spec.ts
+++ src/lib/civitai.spec.ts
@@ -1,38 +1,40 @@
-import fetchMock from "jest-fetch-mock";
-fetchMock.enableMocks();
-
-import { describe, expect, test as it } from "@jest/globals";
-import createCachingMock from "jest-fetch-mock-cache";
-import NodeFsStore from "jest-fetch-mock-cache/lib/stores/nodeFs";
+import { describe, expect, test as it, vi } from "vitest";
+import createFetchCache from "fetch-mock-cache";
+import Store from "fetch-mock-cache/stores/fs";
 
 import {
-  fetchModel,
   extractModelId,
+  fetchModel,
   modelIdFromIdOrUrlOrHash,
 } from "./civitai";
 
-fetchMock.mockImplementation(createCachingMock({ store: new NodeFsStore() }));
+const fetchCache = createFetchCache({ Store });
 
 describe("CivitAI", () => {
   describe("fetchModel", () => {
     it("works", async () => {
+      vi.stubGlobal("fetch", fetchCache);
       const model = await fetchModel(99201);
       expect(model.id).toBe(99201);
+      vi.unstubAllGlobals();
     });
   });
 
   describe("extractModelId", () => {
     it("works", () => {
+      vi.stubGlobal("fetch", fetchCache);
       expect(extractModelId("99201")).toBe("99201");
       expect(
-        extractModelId("https://civitai.com/models/99201/angelina-jolie-jg")
+        extractModelId("https://civitai.com/models/99201/angelina-jolie-jg"),
       ).toBe("99201");
       expect(extractModelId("https://civitai.com/models/99201")).toBe("99201");
+      vi.unstubAllGlobals();
     });
   });
 
   describe("modelIdFromIdOrUrlOrHash", () => {
     it("works", async () => {
+      vi.stubGlobal("fetch", fetchCache);
       const hashes = [
         "7BFDB20388",
         "7BFDB20388CD3511DB6FB0B0D0F2868729B9FCA121A1314C7EBB1B180FA6B43D",
@@ -43,6 +45,7 @@ describe("CivitAI", () => {
       for (const hash of hashes) {
         expect(await modelIdFromIdOrUrlOrHash(hash)).toBe(103638);
       }
+      vi.unstubAllGlobals();
     });
   });
 });
--- src/sd/utils.spec.ts
+++ src/sd/utils.spec.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from "vitest";
 import { prompt_a111_to_compel } from "./utils";
 
 describe("utils", () => {
